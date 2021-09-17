232d7e8f59529be27991147a46f9ffee3db55f14
Quang Nguyen <quangpmnguyen@gmail.com>
2021-09-17 14:10:50 -0400

    Isolating renv environment

--- R/explore.R
+++ R/explore.R
@@ -1,8 +1,8 @@
 library(tidyverse)
 
 weissman <- read_csv(file = "data/weissman.csv")
-
-head(weissman)
+madin_traits <- read_csv(file = "data/madin_condensed_traits_NCBI.csv")
+madin_species <- read_csv(file = "data/madin_condensed_species_NCBI.csv")
 
 metadata <- colnames(weissman)[1:19]
 
@@ -12,3 +12,26 @@ w_db <- weissman |>
                names_to = "names", values_to = "values") |> 
   separate(names, c("class", "trait"), sep = "\\.\\.")
 
+
+metadata <- colnames(madin_traits)[1:11]
+mt_db <- madin_traits |> 
+  select(c(all_of(metadata), c("metabolism", "pathways", "carbon_substrates")))
+
+metadata <- colnames(madin_species)[1:9]
+ms_db <- madin_species |> 
+  select(c(all_of(metadata), c("metabolism", "pathways", "carbon_substrates")))
+
+ms_db |> pull(carbon_substrates) |> unique()
+
+substrates <- ms_db |> pull(carbon_substrates) |> unique()
+
+substrates <- sapply(substrates, function(x) str_split(x, pattern = ","))
+
+substrates <- flatten_chr(substrates) |> unique()
+
+
+pathways <- ms_db |> pull(pathways) |> unique()
+
+pathways <- sapply(pathways, function(x) str_split(x, pattern = ","))
+
+pathways <- flatten_chr(pathways) |> unique()
