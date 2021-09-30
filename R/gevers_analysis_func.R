f8f39f3dcb5b153ba5b20df8e9644e236edb9134
Quang Nguyen <quang.p.nguyen.gr@dartmouth.edu>
2021-09-29 21:13:48 -0400

    Adding metacyc profiling

--- R/gevers_analysis_func.R
+++ R/gevers_analysis_func.R
@@ -7,24 +7,45 @@ if (!file.exists(tdb_cache$list())){
   taxizedb::db_download_ncbi(verbose = FALSE, overwrite = FALSE)
 }
 
+# call ncbi ids 
+# TODO: Modfiy to just return the entire vector 
+call_id <- function(ids){
+  ncbi <- map_chr(ids, ~{
+    res <- name2taxid(.x, out_type = "summary")
+    return(as.character(res[1,2]))
+  })
+  return(ncbi)
+}
+
+
 #' @param trait_vec Vector of unique traits 
 #' @param db Database where species/taxa as rows and traits as columns.
 #' @param filt_term Term to filter database on 
 #' @param id_col Name of id column indicating NCBI identifiers 
 #' @param genus_agg What are the direct levels 
 #' @param threshold Threshold for when a trait is considered
-retr_sets <- function(db, trait_vec, filt_term, id_col, genus_agg, threshold = 0.95){
-  if (filt_term %in% c("carbon_substrates", "pathways")){
-    set_list <- map(trait_vec, ~{
-      db |> filter(.x %in% !!sym(filt_term)) |> 
-        pull(!!sym(id_col)) |> as.character()
-    })
-  } else {
-    set_list <- map(trait_vec, ~{
-      db |> filter(!!sym(filt_term) == .x) |> 
-        pull(!!sym(id_col)) |> as.character()
-    })
-  }
+retr_sets <- function(db, trait_vec, filt_term, id_col, genus_agg = FALSE, threshold = 0.95){
+  # special processing if filtered term is carbon_substrates, pathways
+  set_list <- map(trait_vec, ~{
+    if (filt_term %in% c("carbon_substrates", "pathways")){
+      db_inter <- db |> 
+        rowwise() |> 
+        mutate(t_bool = str_detect(string = !!sym(filt_term), pattern = .x)) |> 
+        ungroup() 
+    } else{
+      db_inter <- db |> mutate(t_bool = !!sym(filt_term) == .x)
+    }
+    if (genus_agg == TRUE){
+      db_inter |> group_by(genus) |> 
+        summarise(t_count = sum(t_bool, na.rm = TRUE), n = n()) |> 
+        mutate(prop = t_count/n) |> 
+        filter(prop >= threshold) |> 
+        mutate(ncbi_id = call_id(genus)) |> 
+        pull(ncbi_id) |> as.character()
+    } else {
+      db_inter |> filter(t_bool == TRUE) |> pull(!!sym(id_col)) |> as.character()
+    }
+  })
   names(set_list) <- trait_vec
   sets <- BiocSet(set_list)
   sets <- sets |> mutate_set(type = filt_term)
@@ -33,7 +54,7 @@ retr_sets <- function(db, trait_vec, filt_term, id_col, genus_agg, threshold = 0
 }
 
 #' This function returns a processed trait_set 
-process_db <- function(){
+process_db <- function(agg = FALSE, thresh = 0.95){
   trait_db <- read.table(file = "data/condensed_species_NCBI.txt", sep = ",", header = TRUE)
   metadata <- colnames(trait_db)[1:8]
   traits <- c("metabolism", "pathways", "carbon_substrates", "sporulation")
@@ -56,11 +77,21 @@ process_db <- function(){
   
   trait_sets <- imap(trait_list, ~{
     retr_sets(db = trait_db, trait_vec = .x, filt_term = traits[.y], 
-              id_col = "species_tax_id")
+              id_col = "species_tax_id", genus_agg = agg, threshold = thresh)
   })
   return(trait_sets)
 }
 
-
-
+process_metacyc <- function(){
+  # change db_path
+  db_path <- file.path("databases", "meta_25.1", "data")
+  if (length(list.files(db_path)) == 0){
+    stop("Need to download metaCyc pathways to db_path")
+  }
+  
+  class_file <- file.path(db_path, "classes.dat")
+  classes <- readr::read_file(class_file)
+  
+  
+}
 
--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Analyses performed using the `targets` package in R. Access `run.R` to run indiv
 
 Individual pipelines include:   
 * `script-dada2.R` defines targets for processing data from Gevers et al. using the `dada2` manuscript.  
+
+
+## Installing the required python packages  
+
+This project uses `reticulate` package to call python functions in R. This is linked to a virtual environment that is managed by `renv` using `requirements.txt`.  To ensure the best usage, please edit `.Renviron` file to force `RETICULATE_PYTHON` to be at the correct virtual environment. This will also correct the behavior of `renv` to point to the correct python binaries.   
+
+In order to conveniently access the MetaCyc pathway class hierarchy, the package `pythoncyc` is used. Since this package is not distributed via `pip` or `conda`, please create a new conda or virtual environment, and then use the [manual installation instructions](https://github.com/ecocyc/PythonCyc) when your desired virtual environment is activated.  
+ 
+
