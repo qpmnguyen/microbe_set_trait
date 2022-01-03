7f5e4227666a5f08dd475da2d0e99a0eaacc7cc2
Quang Nguyen <quangpmnguyen@gmail.com>
2022-01-02 21:06:48 -0500

    Fixing and prototyping some issues

--- R/db_preprocess.R
+++ R/db_preprocess.R
@@ -1,215 +1,95 @@
 # scripts to pre-process and match the metacyc and
 # taxonomic databases
-library(tidyverse)
+library(data.table)
+library(magrittr)
 library(phyloseq)
 library(BiocSet)
 library(taxizedb)
-library(reticulate)
 library(glue)
+library(taxadb)
+library(purrr)
+library(DBI)
+library(stringr)
+library(dplyr)
 library(dbplyr)
+library(tidyr)
 
 if (length(tdb_cache$list()) == 0) {
     taxizedb::db_download_ncbi(verbose = FALSE, overwrite = FALSE)
 }
 
-# call ncbi ids
-#' @param str_ids A list of vectors of names from superkingdom to genus level
-#' @param t_rank Unused argument for future control of which ranks to call on the ids
-call_id <- function(str_ids, t_rank = "genus") {
-    # get genus identifiers
-    ids <- map_chr(str_ids, ~ {
-        .x[length(.x)]
-    })
-    # map over genus names
-    ncbi <- map(ids, ~ {
-        if (is.na(.x)) {
-            return(NA_character_)
-        } else {
-            res <- name2taxid(.x, out_type = "summary")
-            # remove anything that's not bacteria
-            ids <- map_chr(res$id, ~ {
-                classif <- classification(.x)
-                superkingdom <- classif[[1]] |>
-                    filter(rank == "superkingdom") |>
-                    pull(name)
-                if (superkingdom == "Bacteria") {
-                    return(.x)
-                } else {
-                    return(NA_character_)
-                }
-            })
-            ids <- as.vector(na.omit(ids))
-            # if after removing bacteria, there are still ids remain
-            if (length(ids) >= 2) {
-                # get the relevant ranks for the classification
-                ranks <- c(
-                    "superkingdom", "phylum", "class",
-                    "order", "family", "genus", "species"
-                )
-                rank_seq <- seq_len(which(ranks == tolower(t_rank)))
-                ranks <- ranks[rank_seq]
-                full_rank <- classification(ids)
-                # test for equivalence for each full rank name
-                rank_test <- map_lgl(full_rank, function(y) {
-                    ref_name <- str_ids
-                    new_name <- y |>
-                        dplyr::filter(rank %in% ranks) |>
-                        dplyr::pull(name)
-                    all(ref_name == new_name)
-                })
+# PROCESS THE TRAIT DATABASE ####   
 
-                # if all rank_test is FALSE, return everything
-                if (sum(rank_test) == 0) {
-                    message("Ambiguous identifiers")
-                    return(ids)
-                } else {
-                    return(ids[rank_test])
-                }
-            } else {
-                return(ids)
-            }
-        }
-    })
-    return(ncbi)
-}
-
-
-#' @param trait_vec Vector of unique traits
-#' @param db Database where species/taxa as rows and traits as columns.
-#' @param filt_term Term to filter database on
-#' @param id_col Name of id column indicating NCBI identifiers
-#' @param genus_agg What are the direct levels
-#' @param threshold Threshold for when a trait is considered
-retr_sets <- function(db, trait_vec, filt_term,
-                      id_col, genus_agg = FALSE, threshold = 0.95) {
-    # special processing if filtered term is carbon_substrates, pathways
-    db_nspec <- nrow(db)
-    set_list <- map(trait_vec, ~ {
-        if (filt_term %in% c("carbon_substrates", "pathways")) {
-            db_inter <- db |>
-                rowwise() |>
-                mutate(t_bool = str_detect(
-                    string = !!sym(filt_term),
-                    pattern = .x
-                )) |>
-                ungroup()
-        } else {
-            db_inter <- db |>
-                mutate(t_bool = ifelse(!!sym(filt_term) == .x, TRUE, FALSE))
-        }
-        if (genus_agg == TRUE) {
-            c_ranks <- c(
-                "superkingdom", "phylum", "class",
-                "order", "family", "genus"
-            )
-
-            # first, filter out genera without trait at at least 0.95 of species
-            print(.x)
-            db_agg <- db_inter |>
-                group_by(across(all_of(c_ranks))) |>
-                summarise(t_count = sum(t_bool, na.rm = TRUE), n = n()) |>
-                filter(t_count / n >= threshold)
-
-            full_name <- pmap(db_agg |> select(-c(t_count, n)), ~ {
-                unname(c(...))
-            })
-            # retreive ncbiids
-            ncbi_ids <- call_id(full_name)
-
-            # get p_values for filter
-            p_values <- imap(ncbi_ids, ~ {
-                test_genus(.x, db_nspec = db_nspec, db_ngenus = db_agg$n[.y])
-            })
-            sig_ids <- map(p_values, proc_gtest)
-            flatten_chr(sig_ids)
-        } else {
-            db_inter |>
-                filter(t_bool == TRUE) |>
-                pull(!!sym(id_col)) |>
-                as.character()
-        }
-    })
-    names(set_list) <- trait_vec
-    sets <- BiocSet(set_list)
-    # remove NAs if there are any
-    sets <- sets |> filter_element(!is.na(element))
-    sets <- sets |> mutate_set(type = filt_term)
-    return(sets)
-}
-
-#' This function returns a processed trait_set
-process_db <- function() {
-    trait_db <- read.table(
-        file = "data/condensed_species_NCBI.txt",
-        sep = ",", header = TRUE
-    )
-    metadata <- colnames(trait_db)[1:8]
-    traits <- c("metabolism", "pathways", "carbon_substrates", "sporulation")
-    # restrict databases to certain types of traits
-    trait_db <- trait_db |>
-        as_tibble() |>
-        select(all_of(metadata), all_of(traits)) |>
-        dplyr::filter(superkingdom == "Bacteria")
-
+#' Read in trait database as a data.table  
+read_trait <- function(){
+    trait_db <- fread(file = "data/condensed_species_NCBI.txt")
     return(trait_db)
 }
 
-#' This function takes the trait database and construct
-get_trait_list <- function(trait_db) {
-    traits <- c("metabolism", "pathways", "carbon_substrates", "sporulation")
-    trait_list <- map(traits, ~ {
-        t_vec <- unique(trait_db |> pull(!!sym(.x)))
-        if (.x %in% c("pathways", "carbon_substrates")) {
-            t_list <- map(t_vec, ~ str_split(.x, ",")) |>
-                flatten() |>
-                flatten_chr()
-            t_vec <- map_chr(t_list, ~ str_trim(.x)) |>
-                unique() |>
-                na.omit() |>
-                as.vector()
-        } else {
-            t_vec <- na.omit(t_vec)
-        }
-        return(t_vec)
-    })
-    return(trait_list)
+#' subset database by trait
+#' @param traitdb The trait database as a data.table 
+#' @param trait A string indicating the desired trait to be extracted 
+get_subset <- function(trait_db, trait) {
+    trait <- match.arg(trait, c("pathways", "carbon_substrates", 
+                                "sporulation", 
+                                "gram_stain", "cell_shape", "range_tmp", 
+                                "range_salinity", 
+                                "motility", "metabolism"))
+    metadata <- c(colnames(trait_db)[1:8])
+    columns <- c(metadata, trait)
+    subset_db <- trait_db[,..columns] %>% .[superkingdom == "Bacteria",,] %>% 
+        .[!is.na(i), ,env = list(i = trait)]
+    return(subset_db)
 }
 
-#' @title Generate sets
-#' @param trait_list List of traits prepared
-#' @param trait_db The database prepared
-#' @param agg Whether we're aggregating to Genus level
-#' @param threshold Step 2 of aggregation threshold for a trait to be assigned
-create_sets <- function(trait_list, trait_db, agg = FALSE, threshold = 0.95) {
-    traits <- c("metabolism", "pathways", "carbon_substrates", "sporulation")
-    trait_sets <- imap(trait_list, ~ {
-        retr_sets(
-            db = trait_db, trait_vec = .x, filt_term = traits[.y],
-            id_col = "species_tax_id", genus_agg = agg, threshold = threshold
-        )
+#' @title Retrieve ncbiids based on unique traits 
+#' @param subset_db The trait database ideally restricted to 
+#'     the trait of interest 
+#' @param trait String indicating the trait of interest 
+get_traits <- function(subset_db, trait){
+    trait <- match.arg(trait, c("pathways", "carbon_substrates", 
+                                "sporulation", 
+                                "gram_stain", "cell_shape", "range_tmp", 
+                                "range_salinity", 
+                                "motility", "metabolism"))
+    idx <- ncol(subset_db)
+    if (trait %in% c("pathways", "carbon_substrates")){
+        subset_db <- subset_db[,j := map(str_split(j, ","), str_trim), 
+                               env = list(j = trait)]
+        trait_list <- subset_db[,j, env = list(j = trait)] %>% 
+            flatten_chr() %>% unique()
+    } else {
+        trait_list <- subset_db[,j, env = list(j = trait)] %>% unique()
+    }
+
+    
+    # go through each trait in the trait list, retrieve a list of NCBI ids  
+    # and then return everything  
+    ncbiid_list <- map(trait_list, function(x){
+        subset_db[map_lgl(i, ~{x %in% .x}),,env = list(i = trait)] %>% 
+            pull(species_tax_id)
     })
+    names(ncbiid_list) <- trait_list
+    return(ncbiid_list)
 }
 
-# do not run if trying to perform this reproducibly
-# this is due to the fact that it requires an external software
-# Please visit pathwaytools page for more information.
-process_metacyc <- function() {
-    # please make sure pathway tools is running in the background
-    # The following commands did not work because it forces
-    # the current R console to be in the pathway-tools program
-    # with no easy way to exit
-    # pthway_tools_exec <- "~/pathway-tools"
-    # system(glue("{dir}/pathway-tools -lisp -python-local-only",
-    #            dir = pthway_tools_exec))
-    Sys.setenv(RETICULATE_PYTHON = "~/miniconda3/envs/microbe_set_trait/bin/python3")
-    reticulate::source_python("python/metacyc.py")
-    # get_hierarchy and get_instances are python functions
-    subclass <- get_hierarchy()
-    inst <- map(subclass, ~ {
-        get_instances(.x)
-    })
-    names(inst) <- subclass
-    return(inst)
+
+#' @title Getting the biocSets based on whether aggregation should be performed 
+#' @param ncbiid_list The list of ncbiids 
+#' @param g_agg Logical indicating whether aggregating to genus level 
+get_sets <- function(ncbiid_list, trait_db, trait, g_agg=TRUE) {
+    if (g_agg == FALSE){
+        ncbiid_list <- map(ncbiid_list, as.character)
+        sets <- BiocSet::BiocSet(ncbiid_list)
+        sizes <- es_elementset(sets) %>% group_by(set) %>% count()
+        sets <- sets %>% mutate_set(type = trait, size = sizes$n)
+    } else {
+
+        
+    }
+    
+    
+    return(0)
 }
 
 #' n_spec is not part of the function since it is an expensive operation that
@@ -239,13 +119,14 @@ test_genus <- function(genus_id, db_nspec, db_ngenus) {
             filter(rank == "species", parent_tax_id == x) |>
             tally() |>
             pull(n)
-        # hypergeometric distribution
+        # hypergeometric distribution test for underrepresentation 
+        # P(X <= x) 
         cum_prob <- phyper(
-            q = db_ngenus - 1,
+            q = db_ngenus,
             m = ncbi_ngenus,
             n = ncbi_nspec - ncbi_ngenus,
             k = db_nspec,
-            lower.tail = FALSE
+            lower.tail = TRUE
         )
         cum_prob
     })
@@ -266,4 +147,111 @@ proc_gtest <- function(pvals) {
     } else {
         return(names(pvals)[sig])
     }
+}
+
+
+
+#' @title Function to perform testing for each set of traits within a trait
+#'     type 
+#' @param id_vec The identifier ncbiids in vector format (NOT A LIST)
+#' @param trait_db The trait database as data.table format
+get_filtered_genus <- function(id_vec, trait_db){
+    c_ranks <- c(
+        "superkingdom", "phylum", "class",
+        "order", "family", "genus"
+    )
+    # first let's get all the counts for the species within a genera 
+    # with the trait  
+    tr_counts <- trait_db[species_tax_id %in% id_vec, .(.N), by = c_ranks]
+    
+    # second let's get all the counts for the genera available in the database 
+    tot_counts <- trait_db[,.(.N), by = c_ranks]
+    
+    # merge them together and calculate proportions  
+    merged_counts <- tot_counts[tr_counts, by = c_ranks][,prop := i.N/N]
+    
+    
+    
+    
+}
+
+
+# PROCESS NAMES INTO NCBIIDS ####
+# call ncbi ids using taxizedb function 
+# Use the clean_names function from taxadb to clean names 
+#' @param vec A vector of names represented 
+#'     by ranks from superkingdom to genus
+
+call_genus_id <- function(vec) {
+    c_ranks <- c(
+        "superkingdom", "phylum", "class",
+        "order", "family", "genus"
+    )
+    # clean names and extract genus
+    vec <- as.matrix(vec)
+    vec <- map_chr(vec, ~taxadb::clean_names(.x))
+    genus <- vec[length(vec)]
+    
+    # retrieve ncbiids
+    id_names <- name2taxid(genus, out_type = "summary")
+    
+    # if there are ambiguous idenfiers
+    if (nrow(id_names) >= 2){
+        message("Ambiguous identifiers")
+        # compare reference with new names and return a logical vector
+        # equal to the length of each match  
+        ref_name <- vec
+        check_names <- map_chr(id_names %>% pull(id), ~{
+            query <- classification(.x, db = "ncbi")[[1]]
+            if (is.data.frame(query) == FALSE){
+                return(NA_character_)
+            } else {
+                new_name <- query %>% 
+                    dplyr::filter(rank %in% c_ranks) %>% 
+                    dplyr::pull(name)
+                matching <- ref_name == new_name 
+                diff <- length(matching) - length(which(matching))
+                # handling cases where there are mismatches
+                if (diff <= 1) {
+                    return(TRUE)
+                } else {
+                    return(FALSE)
+                }
+            }
+        })
+        # filter out all false 
+        id_names <- id_names[which(check_names == TRUE & !is.na(check_names))]
+        # if there is still length larger than 1, return the top option 
+        if (nrow(id_names) >= 2){
+            id_names <- id_names$id[1]
+        } else {
+            id_names <- id_names$id
+        }
+    } else {
+        id_names <- id_names$id
+    }
+    return(id_names)
+}
+
+# PROCESS NCBIID RESULTS ####  
+# do not run if trying to perform this reproducibly
+# this is due to the fact that it requires an external software
+# Please visit pathwaytools page for more information.
+process_metacyc <- function() {
+    # please make sure pathway tools is running in the background
+    # The following commands did not work because it forces
+    # the current R console to be in the pathway-tools program
+    # with no easy way to exit
+    # pthway_tools_exec <- "~/pathway-tools"
+    # system(glue("{dir}/pathway-tools -lisp -python-local-only",
+    #            dir = pthway_tools_exec))
+    Sys.setenv(RETICULATE_PYTHON = "~/miniconda3/envs/microbe_set_trait/bin/python3")
+    reticulate::source_python("python/metacyc.py")
+    # get_hierarchy and get_instances are python functions
+    subclass <- get_hierarchy()
+    inst <- map(subclass, ~ {
+        get_instances(.x)
+    })
+    names(inst) <- subclass
+    return(inst)
 }
--- script_db_preprocess.R
+++ script_db_preprocess.R
@@ -1,11 +1,33 @@
 library(targets)
 library(tarchetypes)
+library(tidyverse)
+library(glue)
 source("R/db_preprocess.R")
 
-path_to_db <- function() {
+db_path <- function() {
     output <- file.path("data", "condensed_species_NCBI.txt")
     return(output)
 }
+
+
+trait_list <- tibble(
+    traits = c("pathways", "carbon_substrates", "sporulation", 
+               "gram_stain", "cell_shape", "range_tmp", "range_salinity", 
+               "motility")
+)
+
+
+file <- tar_target(raw_db_file, db_load(), format = "file")
+db <- tar_target(db_file, fread(raw_db_file))
+create_db <- tar_map(unlist = FALSE, values = )
+
+
+list(
+    file,
+    db
+)
+
+
 
 list(
     tar_target(r_db, path_to_db(), format = "file"),
