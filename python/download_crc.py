13ba47c5e0ae795e270994f43a57ab81fe0e0b58
qpmnguyen <quangpmnguyen@gmail.com>
2022-03-30 23:03:56 -0400

    New metadata and files

---
+++ python/download_crc.py
@@ -0,0 +1,51 @@
+from tqdm.autonotebook import tqdm
+import pandas as pd 
+import os
+import matplotlib.pyplot as plt
+import subprocess
+from multiprocessing import Pool
+import time
+
+
+def download_sample(idx):
+    failed = []
+    for j in range(0,2):
+        url = manifest.iloc[idx].fastq_ftp.split(";")[j]
+        sname = sname = manifest.iloc[idx].sample_title.split(" ")[-1]
+        if j == 1:
+            fname = sname + "_R2_001.fastq.gz"
+        else:
+            fname = sname + "_R1_001.fastq.gz"
+        fullname = crc_path + "/" + fname
+        cmd = "wget {} -O {}".format(url, crc_path + "/" + fname)
+        if os.path.exists(fullname):
+            pass
+        else:
+            print("Downloading {}".format(fname))
+            if idx % 10 == 0 & idx >= 10:
+                time.sleep(10)
+            subprocess.run(args=["wget", url, "-O", fullname, "--quiet"], stdout=subprocess.DEVNULL)
+            if os.path.getsize(fullname) == 0:
+                print("For some reason this is not downloading, retrying...")
+                subprocess.run(args=["wget", url, "-O", fullname, "--quiet"], stdout=subprocess.DEVNULL)
+                if os.path.getsize(fullname) == 0:
+                    print("This file is dud")
+                    failed.append(url)
+                    pass
+        return(failed)
+
+
+
+if __name__ == "__main__":
+    basepath = "/dartfs-hpc/rc/lab/H/HoenA/Lab/QNguyen/ResultsFiles/data"
+    asp_key = "/dartfs-hpc/rc/home/k/f00345k/.aspera/connect/etc/asperaweb_id_dsa.openssh"
+    asp_cmd = "ascp -k 1 -QT -l 300m -P33001 -i" 
+    manifest = pd.read_csv("crc_16s.tsv", sep="\t")
+    manifest = manifest[manifest.library_strategy == "AMPLICON"]
+    manifest = manifest[manifest.groupby("sample_title")['read_count'].transform('max') == manifest['read_count']]
+    manifest = manifest.reset_index().drop('index', axis = 1)
+    crc_path = basepath + "/crc_16s"
+    pool = Pool(5)
+    pool.map(download_sample, range(0, manifest.shape[0]))
+    pool.close()
+    pool.join()
